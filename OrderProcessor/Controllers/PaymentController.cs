da17dee3ae1148fbd721fb67c9b5adc198f67cd1
Krishna Agarwal <agarwal.krishna240@gmail.com>
2021-10-23 13:59:42 +0530

    Intial commit - Empty base porject

---
+++ OrderProcessor/Controllers/PaymentController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OrderProcessor.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class PaymentController : ControllerBase
+    {
+        public PaymentController()
+        {
+        }
+
+        [HttpPost]
+        public IActionResult Process()
+        {
+            try
+            {
+                return Ok();
+            }
+            catch
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError);
+            }
+        }
+    }
+}
