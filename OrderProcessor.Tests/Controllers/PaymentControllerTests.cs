daf5ecc286d635c637703668a2a7371751e198a2
Krishna Agarwal <agarwal.krishna240@gmail.com>
2021-10-23 15:49:10 +0530

    Included Nunit and Moq. Included tests for Controller and RuleEngine

---
+++ OrderProcessor.Tests/Controllers/PaymentControllerTests.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Moq;
+using NUnit.Framework;
+using OrderProcessor.Controllers;
+using OrderProcessor.Models;
+using OrderProcessor.RuleEngines;
+using System;
+
+namespace OrderProcessor.Tests.Controllers
+{
+    [TestFixture]
+    class PaymentControllerTests
+    {
+        Mock<IPaymentRuleEngine> _ruleEngine;
+        Mock<PaymentController> _controller;
+
+        [SetUp]
+        public void Setup()
+        {
+            _ruleEngine = new Mock<IPaymentRuleEngine>(MockBehavior.Strict);
+            _controller = new Mock<PaymentController>(_ruleEngine.Object) { CallBase = true };
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            _ruleEngine.VerifyAll();
+        }
+
+        [Test]
+        public void Validate_Process_returns_200_responsecode()
+        {
+            //Arrange
+            var order = new Order();
+            _ruleEngine.Setup(e => e.ExecuteRules(order));
+
+            //Act
+            var result = _controller.Object.Process(order);
+            var okResult = result as OkObjectResult;
+
+            //Assert
+            _ruleEngine.Verify(e => e.ExecuteRules(order), Times.Once);
+            Assert.That(okResult, Is.Not.Null);
+            Assert.That(okResult.StatusCode, Is.EqualTo(200));
+        }
+
+        [Test]
+        public void Validate_Process_returns_500_responseCode()
+        {
+            //Arrange
+            var order = new Order();
+            _ruleEngine.Setup(e => e.ExecuteRules(order)).Throws(new Exception());
+
+            //Act
+            var result = _controller.Object.Process(order);
+
+            //Assert
+            Assert.That((result as StatusCodeResult).StatusCode, Is.EqualTo(500));
+        }
+    }
+}
---
+++ OrderProcessor.Tests/RuleEngine/PaymentRuleEngineTests.cs
@@ -0,0 +1,57 @@
+﻿using Moq;
+using NUnit.Framework;
+using OrderProcessor.Models;
+using OrderProcessor.RuleEngines;
+using OrderProcessor.Rules;
+using System.Collections.Generic;
+
+namespace OrderProcessor.Tests.RuleEngine
+{
+    [TestFixture]
+    public class PaymentRuleEngineTests
+    {
+        Mock<PaymentRuleEngine> _ruleEngine;
+        Mock<PaymentRuleBase> _rule;
+        List<PaymentRuleBase> _rules;
+
+        [SetUp]
+        public void Setup()
+        {
+            _rule = new Mock<PaymentRuleBase>();
+            _rules = new List<PaymentRuleBase>();
+            _rules.Add(_rule.Object);
+
+            _ruleEngine = new Mock<PaymentRuleEngine>(_rules) { CallBase = true };
+        }
+
+        [Test]
+        public void Validate_ExecuteRules_should_invoke_Rule_Execute_method()
+        {
+            //Arrange
+            var order = new Order();
+            _rule.Setup(r => r.IsMatch(order)).Returns(true);
+            _rule.Setup(r => r.Execute(order));
+
+            //Act
+            _ruleEngine.Object.ExecuteRules(order);
+
+            //Assert
+            _rule.Verify(r => r.Execute(order), Times.Once);
+        }
+
+        [Test]
+        public void Validate_ExecuteRules_should_NOT_invoke_Rule_Execute_method()
+        {
+            //Arrange
+            var order = new Order();
+            _rule.Setup(r => r.IsMatch(order)).Returns(false);
+            _rule.Setup(r => r.Execute(order));
+
+            //Act
+            _ruleEngine.Object.ExecuteRules(order);
+
+            //Assert
+            _rule.Verify(r => r.Execute(order), Times.Never);
+        }
+    }
+}
